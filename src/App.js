ebea2bb821cec6a33010426f2d39ce81ba78f5e1
Alaa Eddine <alaneddin91@gmail.com>
2020-11-20 11:24:36 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import logo from './logo.svg';
-
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Carousel } from 'react-bootstrap'
 import './App.css';
 import './style.css';
 
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-       gfhsfhdfhsdhsdhsdfhsfd
+class App extends React.Component {
+
+  state = {
+    movies: []
+  }
+  componentDidMount = async () => {
+    try {
+       let response = await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=ee4589ef&s=fast")
+       let movies = await response.json()
+      console.log(movies[0])
+      this.setState({ movies: movies})
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  render () {
+    return (
   
-      </header>
-    </div>
-  );
+      <div className="App">
+        {movies.map((movie) => {
+          console.log(movie)
+
+        
+        })}
+      </div>
+    
+    );
+  }
 }
 
 export default App;
