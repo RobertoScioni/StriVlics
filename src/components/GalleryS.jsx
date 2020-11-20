da3b0c729776c295a247d652e423632305bfe7c2
Roberto <72069799+RobertoScioni@users.noreply.github.com>
2020-11-20 16:51:50 +0100

    Update GalleryS.jsx
    added exception handling

--- src/components/GalleryS.jsx
+++ src/components/GalleryS.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import "bootstrap/dist/css/bootstrap.min.css"
 
-import { CardGroup, Card, Row, Col } from "react-bootstrap"
+import { CardGroup, Card, Row, Col, Alert } from "react-bootstrap"
 
 class Gallery extends React.Component {
 	state = {
@@ -12,33 +12,37 @@ class Gallery extends React.Component {
 		this.setState({ movies: this.props.movies })
 	}
 	render() {
-		return (
-			<Row>
-				{this.state.movies.map((movie, index) => (
-					<Col
-						className="d-flex justify-content-center mt-2 mb-5"
-						md={5}
-						lg={4}
-						key={index}
-					>
-						<Card>
-							<Card.Img
-								variant="top"
-								style={{ width: 300, height: 400 }}
-								src={movie.Poster}
-							/>
-							<Card.Body>
-								<Card.Title>{movie.Title}</Card.Title>
-								<Card.Text>{movie.Type}</Card.Text>
-							</Card.Body>
-							<Card.Footer>
-								<small className="text-muted">{movie.Year}</small>
-							</Card.Footer>
-						</Card>
-					</Col>
-				))}
-			</Row>
-		)
+		if (this.props.movies) {
+			return (
+				<Row>
+					{this.state.movies.map((movie, index) => (
+						<Col
+							className="d-flex justify-content-center mt-2 mb-5"
+							md={5}
+							lg={4}
+							key={index}
+						>
+							<Card>
+								<Card.Img
+									variant="top"
+									style={{ width: 300, height: 400 }}
+									src={movie.Poster}
+								/>
+								<Card.Body>
+									<Card.Title>{movie.Title}</Card.Title>
+									<Card.Text>{movie.Type}</Card.Text>
+								</Card.Body>
+								<Card.Footer>
+									<small className="text-muted">{movie.Year}</small>
+								</Card.Footer>
+							</Card>
+						</Col>
+					))}
+				</Row>
+			)
+		} else {
+			return <Alert variant="danger">ERROR NO RESULTS FOUND</Alert>
+		}
 	}
 }
 
