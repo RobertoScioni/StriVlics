4e50065b520eb2166f91cb2bcd65e69e3a642e17
Alaa Eddine <alaneddin91@gmail.com>
2020-11-20 13:14:41 +0100

    fetched movies

--- src/App.js
+++ src/App.js
@@ -1,39 +1,22 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Carousel } from 'react-bootstrap'
-import './App.css';
 import './style.css';
+import Gallery from './components/Gallery'
+
+
 
 
 
 class App extends React.Component {
-
-  state = {
-    movies: []
-  }
-  componentDidMount = async () => {
-    try {
-       let response = await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=ee4589ef&s=fast")
-       let movies = await response.json()
-      console.log(movies[0])
-      this.setState({ movies: movies})
-    } catch (error) {
-      console.log(error)
-    }
-  }
   render () {
-    return (
-  
-      <div className="App">
-        {movies.map((movie) => {
-          console.log(movie)
-
-        
-        })}
-      </div>
     
-    );
+    return (
+
+      <Gallery search="fast" type="movie" />
+    )
   }
+
+  
 }
 
 export default App;
